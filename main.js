e6b2f8acc3c35b1592429fbcc49be6c1e7790f8f
Seth Childers <sethchilders92@gmail.com>
2018-02-14 13:17:41 -0700

    Got the course description and title, added 'npm he' to the module to decode html, need to get rid of html tags concatenated onto title and then stick it into the title spot and delete the description

--- main.js
+++ main.js
@@ -1,42 +1,63 @@
 /*eslint-env node, es6*/
+/*eslint no-console:1*/
 
 /* Module Description */
 
 /* Put dependencies here */
 
 /* Include this line only if you are going to use Canvas API */
-const canvas = require('canvas-wrapper');
+/*const canvas = require('canvas-wrapper'); np*/
+const he = require('he');
 
 /* View available course object functions */
 // https://github.com/byuitechops/d2l-to-canvas-conversion-tool/blob/master/documentation/classFunctions.md
 
 module.exports = (course, stepCallback) => {
 
-	/********************************
-	 *          STARTS HERE         *
-	 ********************************/
-	canvas.getModules(course.info.canvasOU, (getModulesErr, moduleList) => {
-		if (getModulesErr) {
-			course.error(`Couldn't retrieve the module list`);
-			stepCallback(null, course);
-			return;
-		}
-		var manifest = course.content.find(file => {
-			return file.name === 'imsmanifest.xml';
-		});
+    /**********************************************
+     * 	getDescriptions()				  
+     *  Parameters: none
+     **********************************************/
+    function getDescriptions() {
+        var manifest = course.content.find(file => {
+            return file.name === 'imsmanifest.xml';
+        });
 
-		/* If grades_d2l.xml wasn't found, stop the child module */
-		if (!manifest) {
-			course.warning('Couldn\'t locate \'imsmanifest.xml\' for this course.');
-			stepCallback(null, course);
-		}
+        /* If grades_d2l.xml wasn't found, stop the child module */
+        if (!manifest) {
+            course.warning('Couldn\'t locate \'imsmanifest.xml\' for this course.');
+            stepCallback(null, course);
+        }
 
-		/* Use Cheerio.js to parse through myFile to find the grading system (points (0), weights (1), or based on a custom formula(2)) */
-		var descriptions = manifest.dom('organization>item');
-		console.log(descriptions);
-		//			if (description.length > 0)
-		//		console.log(description.attr('description'));
+        // var descriptionedItems = [];
 
-		//		var gradingSystem = myFile.dom('configuration>grading_system').html();
-	});
+        var $ = manifest.dom;
+        /* Make an array of each description that isn't empty */
+        $('item').each((i, eachItem) => {
+            //            console.log('before encode:' + eachItem.attribs.description)
+            var description = he.decode(eachItem.attribs.description);
+            // if the description is not empty, check for the number of words in it
+            if (description !== '') {
+                //number of words in the description
+                var numWords = description.split(/\s+|&nbsp;/).length;
+                //if description <=5 words, append to title
+                if (numWords < 6) {
+                    //get the title name, then add the description to it;
+                    var title = $(eachItem).find('title').html();
+                    console.log(title);
+                    title.concat(description);
+                    //need to get rid of html tags and just get the text
+                    console.log(title.concat(description));
+                    //else delete the description
+                } else {
+                    console.log(`delete the description here`);
+                }
+            }
+        });
+    }
+
+    /********************************
+     *          STARTS HERE         *
+     ********************************/
+    getDescriptions();
 };
